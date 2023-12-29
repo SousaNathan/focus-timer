3240d87bcb1d401940d443b2d47620f0c375017f
Nathan Sousa <nathan.wks@outlook.com>
2023-12-28 23:10:12 -0300

    state actions e elements

---
+++ src/FocusTimer/events.js
@@ -0,0 +1,14 @@
+import { controls } from "./elements.js"
+import * as actions from './actions.js'
+
+export function registerControls() {
+  controls.addEventListener('click', (event) => {
+    const action = event.target.dataset.action
+
+    if (typeof actions[action] != "function") {
+      return
+    }
+
+    actions[action]()
+  })
+}
